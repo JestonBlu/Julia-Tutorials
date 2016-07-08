1ca019911b31af89219e05f0c42f68c60538c655
JestonBlu <jestonblu@gmail.com>
2016-07-08 13:01:19 -0500

    removed non julia files

--- regression.jl
+++ regression.jl
@@ -1,17 +1,26 @@
 using Gadfly
 
 ## Response Variable
-y = [.25, .13, .65, .4, 1.2]
+Y = [.25, .13, .65, .4, 1.1]
 
 ## Design Matrix
-x = reshape([ones(Int, 5); collect(1:1:5)], 5, 2)
+X = reshape([ones(Int, 5); collect(1:1:5)], 5, 2)
+
+## Projection Matrix
+β = inv(X' * X) * X'
 
 ## Solve Betas
-B = inv(x' * x) * x' * y
+β * Y
 
 ## Calculate Predicted values
-x * B
+y = X * (β * Y)
 
 ## Plot regression
-plot(layer(x = x[:,2], y = y, Geom.point),
-     layer(x = x[:,2], y = x*B, Geom.line))
+plot(layer(x = X[:,2], y = Y, Geom.point),
+     layer(x = X[:,2], y = y, Geom.line))
+
+## Hat Matrix
+H = X * β
+
+## Leverage Points
+diag(H)

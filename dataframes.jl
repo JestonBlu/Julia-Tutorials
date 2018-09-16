2a09cd441ad12e6297750c46a94f267322646dac
JestonBlu <jestonblu@gmail.com>
2018-09-16 07:05:33 -0500

    messing around

--- dataframes.jl
+++ dataframes.jl
@@ -13,3 +13,18 @@ iris[:, 1:3]
 
 # operations across columns
 colwise(sum, iris[:,1:3])
+
+# build a dataframe from scratch, quotations vs ticks matters
+DataFrame(A = 1:4, B = ["a", "b", "c", "d"])
+
+# Dataframe dimensions
+size(iris)
+size(iris)[1]
+size(iris)[2]
+
+# manually adding a new row
+push!(iris, [0, 0, 0, 0, "other"])
+
+# end and beginning of dataset
+tail(iris)
+head(iris)

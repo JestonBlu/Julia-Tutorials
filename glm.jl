1fe95d9368d1f44953c227471686a40c99585161
jestonblu <jestonblu@gmail.com>
2018-12-21 15:55:05 -0600

    basic glm

--- glm.jl
+++ glm.jl
@@ -3,3 +3,12 @@ using DataFrames
 using GLM
 
 dta = dataset("datasets", "iris")
+
+mdl = lm(@formula(SepalLength ~ PetalLength + Species), dta)
+
+# Standard Errors and predictions
+stderror(mdl)
+predict(mdl)
+confint(mdl)
+
+mdl = glm(@formula(SepalLength ~ PetalLength + Species), dta, Normal())

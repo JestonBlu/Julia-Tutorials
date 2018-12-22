55efd9c107ea3c67a6aea63ff21dc37c7721da67
jestonblu <jestonblu@gmail.com>
2018-12-22 13:21:05 -0600

    update plots

--- plots.jl
+++ plots.jl
@@ -11,5 +11,39 @@ plotly()
 plot(rand(5))
 
 # Unicode plots, writes plots to console
+# Pretty much limited to lines and scatters
 unicodeplots()
 plot(rand(5))
+
+# using GR, no dependencies
+gr()
+
+# assumes x and y
+plot(rand(5), rand(5))
+
+# assumes x=1:5, 2 y series
+plot([rand(10), rand(10)])
+# equivalent
+plot(rand(10, 2))
+
+scatter(rand(10,2))
+scatter(rand(10,2), rand(10,2))
+
+using Distributions, StatPlots
+d = Normal()
+
+# Plots using StatPlots Recipe
+plot(d)
+
+x = rand(d, 1000)
+y = rand(d, 1000)
+
+scatter(x, y, title = "Random Normal Samples")
+histogram(x)
+
+# Plotting with dataframes
+using RDatasets
+
+dta = dataset("datasets", "iris")
+
+@df dta scatter(:SepalLength, :SepalWidth, group = :Species)
